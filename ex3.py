f4c9ef49ec168fb5689f6f9e59cbbb22dbb1f67f
kyle <kyledavv@gmail.com>
2018-11-12 14:53:04 -0600

    ex3.py

--- ex3.py
+++ ex3.py
@@ -4,7 +4,7 @@ print("Hens", 25 + 30 / 6)
 print("Roosters", 100 - 25 * 3 % 4)
 # - is subtraction, * is multiplication, % is modulus (x / y with z remaining.)
 print("Now I will count the eggs:")
-# follows PEMDAS. 
+# follows PEMDAS.
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
 print("Is it true that 3 + 2 < 5 - 7?")
